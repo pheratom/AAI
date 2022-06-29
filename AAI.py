4f784d1902cb9099fee0cc2c9df932bc01ce2495
Pheratom <108405313+pheratom@users.noreply.github.com>
2022-06-29 11:59:15 +0000

    Initial Release 1.0

---
+++ AAI.py
@@ -0,0 +1,99 @@
+import os
+import time
+import colorama
+import sys
+from colorama import Fore, Back, Style
+try:
+    from appInstaller import Install
+except ModuleNotFoundError:
+    print(f"{Fore.RED}You did't installed requirement packages.")
+    answer = input(f"Install packages? (y/n) ")
+    if answer == 'y':
+        os.system('python3 -m pip install wget && python3 -m pip install colorama')
+        pass
+    elif answer == 'n':
+        print(f"Ok, exitting...")
+        sys.exit()
+    else:
+        print("You need to type 'y' or 'n'. Nothing else!")
+        sys.exit()
+else:
+    from appInstaller import Install
+colorama.init(autoreset = True)
+def printAppList():
+    print(f""" {Fore.CYAN}
+Select app: {Fore.BLUE}
+\t0 - Install App Listed Apps
+\t1 - Librewolf
+\t2 - Telegram
+\t3 - Veracrypt
+\t4 - Virtualbox
+\t5 - KeePassXC
+\t6 - Discord
+\t7 - QBitTorrent
+\t8 - Wireshark
+\t9 - NordVPN
+\t10 - GParted
+\t11 - Gnome Tweaks
+\t12 - ZSH + PowerLevel10k
+\t13 - ProtonVPN
+\t14 - Nala (APT FrontEnd)
+\t99 - Exit
+""")
+
+if __name__ == '__main__':
+    print(Fore.GREEN + '''
+                     _____        __ 
+     /\        /\   |_   _|      /_ |
+    /  \      /  \    | |   __   _| |
+   / /\ \    / /\ \   | |   \ \ / / |
+  / ____ \  / ____ \ _| |_   \ V /| |
+ /_/    \_\/_/    \_\_____|   \_(_)_|''') #https://www.fontchanger.net/ascii-text.html Font - Big
+    while True:
+        printAppList()
+        try:
+            number = int(input(f"{Style.BRIGHT}Enter number: {Style.NORMAL}"))
+        except ValueError:
+            print(f"{Style.BRIGHT}{Fore.RED}You must enter number.")
+            pass
+        except KeyboardInterrupt:
+            print(f"{Style.BRIGHT}{Fore.RED}\nJust enter 99 to exit!")
+            time.sleep(0.5)
+            pass
+        except:
+            print(f"{Style.BRIGHT}{Fore.BLACK}Unknown Error!")
+        else:
+            if number == 99:
+                break
+            elif number == 0:
+                Install.all()
+            elif number == 1:
+                Install.librewolf()
+            elif number == 2:
+                Install.telegram()
+            elif number == 3:
+                Install.veracrypt()
+            elif number == 4:
+                Install.virtualbox()
+            elif number == 5:
+                Install.keepassxc()
+            elif number == 6:
+                Install.discord()
+            elif number == 7:
+                Install.qbittorrent()
+            elif number == 8:
+                Install.wireshark()
+            elif number == 9:
+                Install.nordvpn()
+            elif number == 10:
+                Install.gparted()
+            elif number == 11:
+                Install.gnometweaks()
+            elif number == 12:
+                Install.zsh_p10k()
+            elif number == 13:
+                Install.protonvpn()
+            elif number == 14:
+                Install.nala()
+            else:
+                print(f"{Style.BRIGHT}{Fore.RED}You entered wrong number!")
---
+++ appInstaller.py
@@ -0,0 +1,109 @@
+import os
+import wget
+import colorama
+from colorama import Fore, Back, Style
+class Install:
+    def __init__(self):
+        pass
+    def all():
+        Install.librewolf()
+        Install.telegram()
+        Install.veracrypt()
+        Install.virtualbox()
+        Install.keepassxc()
+        Install.discord()
+        Install.qbittorrent()
+        Install.wireshark()
+        Install.nordvpn()
+        Install.gparted()
+        Install.gnometweaks()
+        Install.zsh_p10k()
+        Install.protonvpn()
+        Install.nala()
+    def librewolf(): #WARNING! It works with Ubuntu 2022.04 (jammy) based distros. Script from librewolf developer don't work properly (or it my mistake), so if you need to use other distro enter instead 'jammy' *other distro name*
+        os.system('echo "deb [arch=amd64] http://deb.librewolf.net jammy main" | sudo tee /etc/apt/sources.list.d/librewolf.list')
+        os.system('sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg')
+        os.system('sudo apt update -y')
+        os.system('sudo apt install librewolf -y')
+
+    def telegram():
+        wget.download('https://telegram.org/dl/desktop/linux', 'telegram.tar.xz')
+        os.system('tar -xf telegram.tar.xz -C ~/')
+        print("\nNow, u can leave app. It added to your app list.")
+        os.system('~/Telegram/Telegram && rm telegram*') #Start Telegram (to add icon in app list) and removing Telegram installer (cleaning workdir)
+
+    def veracrypt():
+        #Getting VeraCrypt Developers public key and verifying it.
+        wget.download('https://www.idrix.fr/VeraCrypt/VeraCrypt_PGP_public_key.asc', 'veracrypt_pub_key.asc')
+        os.system('gpg --import veracrypt_pub_key.asc')
+        url = 'https://launchpad.net/veracrypt/trunk/1.25.9/+download/veracrypt-1.25.9-Ubuntu-22.04-amd64.deb'
+        wget.download(url, 'veracrypt.deb')
+        url = 'https://launchpad.net/veracrypt/trunk/1.25.9/+download/veracrypt-1.25.9-Ubuntu-22.04-amd64.deb.sig'
+        wget.download(url, 'veracrypt.deb.sig')
+        os.system('gpg --verify veracrypt.deb.sig')
+        input("Please, check that it Good Signature and press any key.")
+        #Installing and cleaning workdir
+        os.system('sudo apt install ./veracrypt.deb -y && rm veracrypt*')
+
+    def virtualbox():
+        os.system('sudo apt install virtualbox -y')
+
+    def keepassxc():
+        os.system('sudo add-apt-repository ppa:phoerious/keepassxc') #Adding official KeePassXC repo
+        os.system('sudo apt update -y')
+        os.system('sudo apt install keepassxc -y') #Installing latest version
+
+    def discord():
+        os.system('curl -L https://discord.com/api/download\?platform\=linux\&format\=deb > discord.deb')
+        os.system('sudo apt install ./discord.deb -y')
+        os.system('rm discord*')
+
+    def qbittorrent():
+        os.system('sudo apt install qbittorrent -y')
+
+    def wireshark():
+        os.system('sudo apt install wireshark -y')
+        os.system('sudo dpkg-reconfigure wireshark-common -y')
+
+    def nordvpn():
+        os.system('curl https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb > nordvpn.deb')
+        #Installing nordvpn and cleaning workdir
+        os.system('sudo apt install ./nordvpn.deb -y && sudo apt update -y && sudo apt install nordvpn -y && rm nordvpn*')
+        print("Recommended to log out user account. (to avoid NordVPN bug.)")
+
+    def gparted():
+        os.system('sudo apt install gparted -y')
+
+    def gnometweaks():
+        os.system('sudo apt install gnome-tweaks -y')
+
+    def zsh_p10k():
+        #Warning about changing terminal bug. When you install zsh it changes terminal to zsh. To return to main menu you need to type 'exit' in terminal
+        print(f"""{Fore.RED}{Style.BRIGHT}
+                                .__            ____  ._. 
+__  _  _______  _______   ____  |__|  ____    / ___\ | | 
+\ \/ \/ /\__  \ \_  __ \ /    \ |  | /    \  / /_/  >| | 
+ \     /  / __ \_|  | \/|   |  \|  ||   |  \ \___  /  \| 
+  \/\_/  (____  /|__|   |___|  /|__||___|  //_____/   __ 
+              \/             \/          \/           \/ """)
+        print('Returning to main menu may be buggy, since while install your terminal changes.')
+        print('Also you may install additional font for your ZSH theme.')
+        print('Enter Enter to continue')
+        input()
+        os.system('sudo apt install zsh curl git -y')
+        os.system('sudo chsh $USER -s /usr/bin/zsh')
+        os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended')
+        os.system('git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k')
+        os.system('rm ~/.zshrc')
+        os.system('cp .zshrc ~/.zshrc')
+        os.system('zsh')
+
+    def protonvpn():
+        wget.download('https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb', 'protonvpn.deb')
+        os.system('sudo apt install ./protonvpn.deb -y && sudo apt update -y && sudo apt install protonvpn -y')
+        os.system('sudo apt install gnome-shell-extension-appindicator gir1.2-appindicator3-0.1 -y')
+        os.system('rm proton*')
+
+    def nala():
+        os.system('echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list && wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null')
+        os.system('sudo apt update -y && sudo apt install nala -y')
