d3f6fe9f79207de9b85f0d01db1cc152eca50f5b
Pheratom <108405313+pheratom@users.noreply.github.com>
2022-07-02 11:38:44 +0000

    v.1.3
    Fixed LibreWolf, VeraCrypt, ZSH+P10k install.
    Now installer (tested in Ubuntu 22.04) 100% automatic.

--- appInstaller.py
+++ appInstaller.py
@@ -1,6 +1,10 @@
 import os
 import wget
 import colorama
+import time
+import sys
+import subprocess
+import requests
 from colorama import Fore, Back, Style
 class Install:
     def __init__(self):
@@ -13,37 +17,57 @@ class Install:
         Install.keepassxc()
         Install.discord()
         Install.qbittorrent()
-        Install.wireshark()
         Install.nordvpn()
         Install.gparted()
         Install.gnometweaks()
-        Install.zsh_p10k()
         Install.protonvpn()
         Install.nala()
-    def librewolf(): #WARNING! It works with Ubuntu 2022.04 (jammy) based distros. Script from librewolf developer don't work properly (or it my mistake), so if you need to use other distro enter instead 'jammy' *other distro name*
-        os.system('echo "deb [arch=amd64] http://deb.librewolf.net jammy main" | sudo tee /etc/apt/sources.list.d/librewolf.list')
+        Install.zsh_p10k()
+    def librewolf(): #It saves {name of linux distro} in temporary file. Then it copies {name of linux distro} in string and add LibreWolf source repo. (because scipts from developers don't work ¯\_(ツ)_/¯).
+        command = "lsb_release -sc > temp.txt"
+        os.system(command)
+        with open('temp.txt', 'r') as file:
+            distro = file.read()
+            distro = distro.strip('\n')
+        if distro != 'bullseye' and distro != 'focal' and distro != 'impish' and distro != 'jammy' and distro != 'uma' and distro != 'una':
+            distro = 'focal'
+        os.system(f'echo "deb [arch=amd64] http://deb.librewolf.net {distro} main" | sudo tee /etc/apt/sources.list.d/librewolf.list')
         os.system('sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg')
         os.system('sudo apt update -y')
         os.system('sudo apt install librewolf -y')
+        os.system('rm temp.txt')
 
     def telegram():
         wget.download('https://telegram.org/dl/desktop/linux', 'telegram.tar.xz')
         os.system('tar -xf telegram.tar.xz -C ~/')
-        print("\nNow, u can leave app. It added to your app list.")
-        os.system('~/Telegram/Telegram && rm telegram*') #Start Telegram (to add icon in app list) and removing Telegram installer (cleaning workdir)
+        os.system('~/Telegram/Telegram &')
+        os.system('rm telegram*')
+        print(f"{Fore.RED}Telegram will close automatically!")
+        time.sleep(5)
+        os.system('sudo pkill Telegram') #Auto add telegram icon in app list
 
     def veracrypt():
         #Getting VeraCrypt Developers public key and verifying it.
-        wget.download('https://www.idrix.fr/VeraCrypt/VeraCrypt_PGP_public_key.asc', 'veracrypt_pub_key.asc')
-        os.system('gpg --import veracrypt_pub_key.asc')
-        url = 'https://launchpad.net/veracrypt/trunk/1.25.9/+download/veracrypt-1.25.9-Ubuntu-22.04-amd64.deb'
-        wget.download(url, 'veracrypt.deb')
-        url = 'https://launchpad.net/veracrypt/trunk/1.25.9/+download/veracrypt-1.25.9-Ubuntu-22.04-amd64.deb.sig'
-        wget.download(url, 'veracrypt.deb.sig')
-        os.system('gpg --verify veracrypt.deb.sig')
-        input("Please, check that it Good Signature and press enter.")
-        #Installing and cleaning workdir
-        os.system('sudo apt install ./veracrypt.deb -y && rm veracrypt*')
+        response = requests.get("https://api.github.com/repos/veracrypt/VeraCrypt/releases/latest")
+        full_version_name = response.json()["name"]
+        full_version_name = full_version_name.split()
+        version = full_version_name[-1]
+        wget.download(f'https://launchpad.net/veracrypt/trunk/{version}/+download/veracrypt-{version}-Ubuntu-22.04-amd64.deb', 'veracrypt.deb')
+        wget.download(f'https://launchpad.net/veracrypt/trunk/{version}/+download/veracrypt-{version}-Ubuntu-22.04-amd64.deb.sig', 'veracrypt.deb.sig')
+        wget.download('https://www.idrix.fr/VeraCrypt/VeraCrypt_PGP_public_key.asc', 'veracryptpublickey.asc')
+        os.system('gpg --import veracryptpublickey.asc')
+        command = "gpg --verify veracrypt.deb.sig 2>temp.txt"
+        os.system(command)
+        with open('temp.txt', 'r') as file:
+            result = file.read()
+        if 'Good signature' in result:
+            print(f"{Fore.GREEN}{Style.BRIGHT}Signature verify success!")
+            os.system('sudo apt install ./veracrypt.deb -y')
+            os.system('rm veracrypt*')
+            os.system('rm temp.txt')
+        elif 'Bad signature' in result:
+            print(f"{Fore.RED}{Style.BRIGHT}Signature verify failed! Somebody can do something bad! Exitting.")
+            sys.exit()
 
     def virtualbox():
         os.system('sudo apt install virtualbox -y')
@@ -54,6 +78,7 @@ class Install:
         os.system('sudo apt install keepassxc -y') #Installing latest version
 
     def discord():
+        os.system('sudo apt install curl -y')
         os.system('curl -L https://discord.com/api/download\?platform\=linux\&format\=deb > discord.deb')
         os.system('sudo apt install ./discord.deb -y')
         os.system('rm discord*')
@@ -61,14 +86,11 @@ class Install:
     def qbittorrent():
         os.system('sudo apt install qbittorrent -y')
 
-    def wireshark():
-        os.system('sudo apt install wireshark -y')
-        os.system('sudo dpkg-reconfigure wireshark-common -y')
-
     def nordvpn():
+        os.system('sudo apt install curl -y')
         os.system('curl https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb > nordvpn.deb')
         #Installing nordvpn and cleaning workdir
-        os.system('sudo apt install ./nordvpn.deb -y && sudo apt update -y && sudo apt install nordvpn -y && rm nordvpn*')
+        os.system('sudo apt install ./nordvpn.deb -y && sudo apt update -y && sudo apt install nordvpn -y && rm nordvpn.deb')
         print("Recommended to log out user account. (to avoid NordVPN bug.)")
 
     def gparted():
@@ -85,13 +107,23 @@ __  _  _______  _______   ____  |__|  ____    / ___\ | |
  \     /  / __ \_|  | \/|   |  \|  ||   |  \ \___  /  \| 
   \/\_/  (____  /|__|   |___|  /|__||___|  //_____/   __ 
               \/             \/          \/           \/ """)
-        print('You may install additional font for your ZSH theme.')
+        print(f'{Back.RED}You may install additional font for your ZSH theme.')
+        print(f'{Back.RED}https://github.com/romkatv/powerlevel10k/blob/master/font.md')
         os.system('sudo apt install zsh curl git -y')
+        os.system('rm -rf ~/.oh-my-zsh')
         os.system('sudo chsh $USER -s /usr/bin/zsh')
         os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended')
         os.system('git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k')
-        os.system('rm ~/.zshrc && cp .zshrc ~/.zshrc')
-        print("{Style.BRIGHT}{Fore.RED}You need to relogin to your account or reboot computer to change your default shell to ZSH.")
+        os.system('rm ~/.zsh*')
+        zsh_content = """
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+plugins=(git)
+source $ZSH/oh-my-zsh.sh"""
+        with open('.zshrc', 'w') as file:
+            file.write(zsh_content)
+        os.system('cp .zshrc ~/.zshrc && rm .zshrc')
+        print(f"{Fore.RED}Sometimes default shell don't change. To avoid it just relogin your account or reboot computer.")
 
     def protonvpn():
         wget.download('https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb', 'protonvpn.deb')
